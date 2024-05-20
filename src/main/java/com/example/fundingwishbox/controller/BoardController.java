82f4b297ac4dc6f3a17c59a39310d4d75dfa1864
Hoo <b01066586906@gamil.com>
2024-05-20 11:55:29 +0900

    Controller class 작성

--- src/main/java/com/example/fundingwishbox/controller/BoardController.java
+++ src/main/java/com/example/fundingwishbox/controller/BoardController.java
@@ -1,11 +1,73 @@
 package com.example.fundingwishbox.controller;
 
 
+import com.example.fundingwishbox.config.PrincipalDetails;
+import com.example.fundingwishbox.dto.BoardDto;
+import com.example.fundingwishbox.entity.User;
+import com.example.fundingwishbox.service.BoardService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.IOException;
 
 @Controller
 @RequiredArgsConstructor
 
 public class BoardController {
+
+    @ModelAttribute
+    public void addAttributes(Model model) {
+        model.addAttribute("loginType", "security-login");
+        model.addAttribute("pageName", "Security 로그인");
+    }
+
+    private final BoardService boardService;
+
+    @GetMapping("/save")
+    public String saveForm() {
+        return "Board/boardSave";
+    }
+
+    @PostMapping("/save")
+    public String save(@ModelAttribute BoardDto boardDto, Authentication authentication) throws IOException {
+        PrincipalDetails principalDetails = (PrincipalDetails) authentication.getPrincipal();
+        User writer = principalDetails.getUser(); // 현재 로그인한 사용자의 User 객체를 가져옵니다.
+        boardService.save(boardDto, writer);
+        return "Board/boardHome";
+    }
+
+    @GetMapping("/{id}")
+    public String findById(@PathVariable Long id, Model model,
+                           @PageableDefault(page = 1) Pageable pageable) {
+
+
+        boardService.updateHits(id);
+        BoardDto boardDto = boardService.findById(id);
+        model.addAttribute("board", boardDto);
+        model.addAttribute("page", pageable.getPageNumber());
+        return "Board/boardDetail";
+    }
+
+
+    @GetMapping("/list/search")
+    public String listForm(Model model, @RequestParam(name = "keyword", required = false) String keyword, @PageableDefault(page = 1) Pageable pageable) {
+        Page<BoardDto> BoardDtoList = boardService.abc(keyword, pageable);
+
+        int blockLimit = 3;
+        int startPage = (((int) (Math.ceil((double) pageable.getPageNumber() / blockLimit))) - 1) * blockLimit + 1;
+        int endPage = ((startPage + blockLimit - 1) < BoardDtoList.getTotalPages()) ? startPage + blockLimit - 1 : BoardDtoList.getTotalPages();
+
+        model.addAttribute("dto", BoardDtoList);
+        model.addAttribute("startPage", startPage);
+        model.addAttribute("endPage", endPage);
+        model.addAttribute("keyword", keyword);
+        return "Board/boardList";
+    }
 }
+
--- src/main/java/com/example/fundingwishbox/controller/UserController.java
+++ src/main/java/com/example/fundingwishbox/controller/UserController.java
@@ -1,11 +1,159 @@
 package com.example.fundingwishbox.controller;
 
 
+import com.example.fundingwishbox.dto.JoinRequest;
+import com.example.fundingwishbox.dto.LoginRequest;
+import com.example.fundingwishbox.dto.UserDto;
+import com.example.fundingwishbox.dto.UserRoleUpdateRequest;
+import com.example.fundingwishbox.entity.User;
+import com.example.fundingwishbox.service.UserService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequiredArgsConstructor
 
 public class UserController {
+
+    private final UserService userService;
+
+    @ModelAttribute
+    public void addAttributes(Model model) {
+        model.addAttribute("loginType", "security-login");
+        model.addAttribute("pageName", "Security 로그인");
+    }
+
+    @GetMapping(value = {"", "/"})
+    public String home(Model model, Authentication auth) throws IllegalAccessException {
+
+        if (auth != null) {
+            User loginUser;
+            try {
+                loginUser = userService.getLoginUserByLoginId(auth.getName());
+            } catch (IllegalAccessException e) {
+                model.addAttribute("errorMessage", "You are blacklisted and cannot log in.");
+                return "redirect:/security-login/login";
+            }
+            if (loginUser != null) {
+                model.addAttribute("nickname", loginUser.getNickname());
+            }
+        }
+
+        return "home";
+    }
+
+    @PostMapping("/join")
+    public String join(@Valid @ModelAttribute JoinRequest joinRequest, BindingResult bindingResult, Model model) {
+
+        // loginId 중복 체크
+        if (userService.checkLoginIdDuplicate(joinRequest.getLoginId())) {
+            bindingResult.addError(new FieldError("joinRequest", "loginId", "로그인 아이디가 중복됩니다."));
+        }
+        // 닉네임 중복 체크
+        if (userService.checkNicknameDuplicate(joinRequest.getNickname())) {
+            bindingResult.addError(new FieldError("joinRequest", "nickname", "닉네임이 중복됩니다."));
+        }
+        // password와 passwordCheck가 같은지 체크
+        if (!joinRequest.getPassword().equals(joinRequest.getPasswordCheck())) {
+            bindingResult.addError(new FieldError("joinRequest", "passwordCheck", "바밀번호가 일치하지 않습니다."));
+        }
+
+        if (bindingResult.hasErrors()) {
+            return "join";
+        }
+
+        userService.join(joinRequest);
+
+        return "redirect:/security-login";
+    }
+
+    @GetMapping("/login")
+    public String loginPage(Model model) {
+
+        model.addAttribute("loginRequest", new LoginRequest());
+        return "login";
+    }
+
+
+    @GetMapping("/info")
+    public String userInfo(Model model, Authentication auth) throws IllegalAccessException {
+
+        User loginUser = userService.getLoginUserByLoginId(auth.getName());
+
+        if (loginUser == null) {
+            return "redirect:/security-login/login";
+        }
+
+        model.addAttribute("user", loginUser);
+        return "info";
+    }
+
+    @GetMapping("/admin")
+    public String adminPage(Model model) {
+
+        return "admin";
+    }
+
+    @GetMapping("/edit")
+    public String detailForm(Model model, Authentication auth) {
+        User user = userService.myInfo(auth.getName());
+        model.addAttribute("userDto", UserDto.of(user));
+        return "edit";
+
+    }
+
+    @PostMapping("/edit")
+    public String detail(@Valid @ModelAttribute UserDto dto, BindingResult bindingResult,
+                         Authentication auth) {
+
+        if (dto.getNowPassword() != null) {
+            if (userService.editValid(dto, bindingResult, auth.getName()).hasErrors()) {
+                return "edit";
+            }
+            userService.edit(dto, auth.getName());
+        }
+
+        return "redirect:/security-login/info";
+
+    }
+
+    @GetMapping("/admin/userList/search")
+    public String search(Model model, @RequestParam(name = "keyword", required = false) String keyword, @PageableDefault(page = 1) Pageable pageable) {
+        Page<UserDto> UserDtoList = userService.searchAndPaging(keyword,pageable);
+
+        int blockLimit = 3;
+        int startPage = (((int)(Math.ceil((double)pageable.getPageNumber() / blockLimit))) - 1) * blockLimit + 1;
+        int endPage = ((startPage + blockLimit - 1) < UserDtoList.getTotalPages()) ? startPage + blockLimit - 1 : UserDtoList.getTotalPages();
+
+        model.addAttribute("dto", UserDtoList);
+        model.addAttribute("startPage", startPage);
+        model.addAttribute("endPage", endPage);
+        model.addAttribute("keyword", keyword);
+
+        return "userList";
+    }
+
+    @GetMapping("/delete/{id}")
+    public String adminDelete(@PathVariable Long id, @RequestParam("currentPage") int currentPage) {
+        userService.deleteById(id);
+        return "redirect:/security-login/admin/userList/search?page=" + currentPage;
+    }
+
+    @PostMapping("/{id}/update-role")
+    public String updateRole(@PathVariable Long id,
+                             @ModelAttribute UserRoleUpdateRequest request,
+                             @RequestParam("currentPage") int currentPage) {
+
+        userService.updateRole(id, request);
+        return "redirect:/security-login/admin/userList/search?page=" + currentPage;
+    }
 }
