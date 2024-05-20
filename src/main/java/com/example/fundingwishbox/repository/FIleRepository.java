f19582d388a752d96f852fef66d282e45bff9c43
Hoo <b01066586906@gamil.com>
2024-05-20 11:54:18 +0900

    User class import

--- src/main/java/com/example/fundingwishbox/repository/FIleRepository.java
+++ src/main/java/com/example/fundingwishbox/repository/FIleRepository.java
@@ -1,8 +1,10 @@
 package com.example.fundingwishbox.repository;
 
 
+import com.example.fundingwishbox.entity.BoardFile;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface FIleRepository {
+public interface FIleRepository extends JpaRepository<BoardFile, Long> {
 }
