7357883b1a449a57c94e6a64ea5e0ab745f5e109
Hoo <b01066586906@gamil.com>
2024-05-20 11:53:37 +0900

    File -> BoardFile 로 변경

--- src/main/java/com/example/fundingwishbox/entity/Board.java
+++ src/main/java/com/example/fundingwishbox/entity/Board.java
@@ -33,10 +33,10 @@ public class Board extends Time{
 
     @OneToMany(mappedBy = "board")
     @JsonIgnore
-    private List<File> files;
+    private List<BoardFile> boardFiles;
 
     @OneToMany(mappedBy = "board", cascade = CascadeType.REMOVE, orphanRemoval = true, fetch = FetchType.LAZY)
-    private List<File> fileEntityList = new ArrayList<>();
+    private List<BoardFile> boardFileEntityList = new ArrayList<>();
 
 
     public static Board toSaveEntity(BoardDto boardDto, User writer) {
--- src/main/java/com/example/fundingwishbox/entity/BoardFile.java
+++ src/main/java/com/example/fundingwishbox/entity/BoardFile.java
@@ -14,7 +14,7 @@ import lombok.NoArgsConstructor;
 @Builder
 @Table(name = "board_file_table")
 
-public class File extends Time{
+public class BoardFile extends Time{
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -27,8 +27,8 @@ public class File extends Time{
     @JoinColumn(name ="board_id")
     private Board board;
 
-    public static File toBoardFileEntity(Board boardEntity, String originalFileName, String storedFileName) {
-        return  File.builder()
+    public static BoardFile toBoardFileEntity(Board boardEntity, String originalFileName, String storedFileName) {
+        return  BoardFile.builder()
                 .originalFileName(originalFileName)
                 .storedFileName(storedFileName)
                 .board(boardEntity)
